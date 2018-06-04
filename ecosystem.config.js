8107835ad5e730fd2f9750a1904d4ff6e6ac62b2
Aftab Baig <aftabaig@gmail.com>
2018-06-04 16:57:03 +0500

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -28,7 +28,7 @@ module.exports = {
       user : 'aftab',
       host : 'tabdeeliapp.ssasoft.com',
       ref  : 'origin/master',
-      repo : 'git@git.ssasoft.com:voter_list.git',
+      repo : 'https://git.ssasoft.com/aftab.akhtar/voter_list.git',
       path : '/home/aftab/voter_list',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env staging'
     }
